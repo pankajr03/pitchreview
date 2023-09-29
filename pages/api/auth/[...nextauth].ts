4926a3957f22ec83983a5680bfc50792a89c6f4e
Pankaj Kumar <pankaj1211ranout@gmail.com>
2023-09-29 16:02:27 +0530

    google account added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -45,10 +45,10 @@ export const authOptions: NextAuthOptions = {
         
       }
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    // }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID as string,
     //   clientSecret: process.env.GITHUB_SECRET as string,
--- pages/login.tsx
+++ pages/login.tsx
@@ -87,6 +87,27 @@ export default function Login() {
 
 
         </div>
+
+        <div className="flex flex-col px-4 py-8 sm:px-16">
+          <Button
+            onClick={() => {
+              signIn("google", {
+                ...(next && next.length > 0 ? { callbackUrl: next } : {}),
+              });
+            }}
+            className="flex justify-center items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+              fill="currentColor"
+              className="h-4 w-4 mr-2"
+            >
+              <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+            </svg>
+            <span>Continue with Google</span>
+          </Button>
+        </div>
         
       </div>
     </div>
