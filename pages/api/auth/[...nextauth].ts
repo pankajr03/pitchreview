b6f69175fc4e574da07d3e14033a154a1029e5bc
Pankaj Kumar <pankaj1211ranout@gmail.com>
2023-09-30 16:16:22 +0530

    secure set to true

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -72,7 +72,7 @@ export const authOptions: NextAuthOptions = {
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
         //domain: VERCEL_DEPLOYMENT ? ".papermark.io" : undefined,
         domain: VERCEL_DEPLOYMENT ? ".vercel.app" : undefined,
-        secure: VERCEL_DEPLOYMENT,
+        secure: true,
       },
     },
   },
